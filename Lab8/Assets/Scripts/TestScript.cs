bfaf281f2638612719c840e2bc39c5a821fb3498
junhaohogc123 <tjunhao17@gmail.com>
2020-11-09 11:51:22 +0800

    update v2

---
+++ Lab8/Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    int score = 0;
+    public GameObject scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.GetComponent<Text>().text = "Score: " + score;
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            score++;
+        }
+    }
+}
